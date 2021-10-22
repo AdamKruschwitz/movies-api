d929110252de337c0c357197f1f1cc1963be4521
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-22 14:41:41 -0700

    improved seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,7 +8,7 @@
 INSERT INTO reviews (movie_id, review) VALUES
     (1, 'Spirited away is a great film about a little girl who gets lost in the spirit world. It was pretty rad if you ask me.'),
     (1, 'I dont like this movie, it was too slow...'),
-    (2, 'FISH-CHAN IS MY WIFE'),
+    (2, 'The characters are like 8, and at the end its like "you gotta love this girl FOREVER or the world literally explodes". You cant put that kind of commitment onto a kid!'),
     (3, 'Fire-chan is my spirit animal, just look at how he eat eggs'),
     (4, 'This movie awooooo- awoken something in me.'),
     (5, 'I tried riding on a broomstick off the roof and now Im sueing.');
